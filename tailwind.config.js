0aa39024c44ee2399adc6a226a5fa6b5d4c03992
sgrachek66 <sgrachek@gmail.com>
2025-01-24 20:09:11 -0600

    updating templates with tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./portfolio/templates/**/*.html',
+            './portfolio/**/*.py',
+  ],
   theme: {
     extend: {},
   },
